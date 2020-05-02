7e15d45c9b8b9f159645755d6bd54d6fec8168af
Dennis <dennis.merken@gmail.com>
2020-05-02 12:07:02 +0200

    fixed virtual function

--- Controller_Server/ConsoleApplication1/ConsoleApplication1/controller.h
+++ Controller_Server/ConsoleApplication1/ConsoleApplication1/controller.h
@@ -5,10 +5,10 @@ public:
 	int controllerType;
 	//write the data to the struct (in deriv classes)
 	//return: if it worked
-	virtual bool updateData();
+	virtual bool updateData() = 0;
 
 	//send data (set it with setters)
-	virtual bool sendData();
+	virtual bool sendData() = 0;
 
 private:
 	controller();
