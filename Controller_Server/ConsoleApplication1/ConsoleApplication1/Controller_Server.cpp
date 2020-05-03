b437982cb7205014e25596aeabe90a40837a5ffb
Dennis <dennis.merken@gmail.com>
2020-05-03 21:53:28 +0200

    added message decoder (no action yet)

--- Controller_Server/ConsoleApplication1/ConsoleApplication1/Controller_Server.cpp
+++ Controller_Server/ConsoleApplication1/ConsoleApplication1/Controller_Server.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <zmq.h>
 #include <Windows.h>
+#include <vector>
 
 #include "controller.h"
 #include "NES.h"
+#include "ZMQHandler.h"
 
 int main(int argc, char* argv[])
 {
@@ -21,13 +23,25 @@ int main(int argc, char* argv[])
 	
 	//NES *myController = new NES("COM5");
 
-	char buffer[500] = "controllerService?>aapje>";					//entire incoming string
-	char* commandPtr, command[15];		//command part
-	char* argumentsPtr, arguments[100];	//arguments part
+	char buffer[500] = "controllerService?>contr1>sRefrese>2000>";					//entire incoming string
+	char commands[3][20];
+	char* currPosPtr = buffer;
+	char* nextPosPtr = buffer;
 	
-	commandPtr = strchr(buffer, '>');
-	argumentsPtr = strchr(commandPtr + 1, '>');
-	std::cout << commandPtr << std::endl;
-	std::cout << sizeof(buffer) << std::endl;
+	for (short i = 0; i < 3; ++i)  //max commands = 3
+	{
+		currPosPtr = strchr(currPosPtr + 1, '>');	//find where the > character is: shifts up each time
+		if (currPosPtr == NULL) { break; }				//for < 3 commands
+
+		nextPosPtr = strchr(currPosPtr + 1, '>');
+		if (nextPosPtr == NULL) { break; }			//to not include the last >
+
+		memcpy(commands[i], currPosPtr + 1, (nextPosPtr - currPosPtr) - 1); //copy the length of the command (excluding the start and end >)
+		commands[i][(nextPosPtr - currPosPtr)-1] = NULL;					//add terminating NULL
+	}
+	std::cout << commands[0] << std::endl;
+	std::cout << commands[1] << std::endl;
+	std::cout << commands[2] << std::endl;
+	
 	//delete myController;
 }
--- Controller_Server/ConsoleApplication1/ConsoleApplication1/ZMQHandler.cpp
+++ Controller_Server/ConsoleApplication1/ConsoleApplication1/ZMQHandler.cpp
@@ -33,6 +33,21 @@ int ZMQHandler::recv()
 	zmq_recv(subPtr, buffer, sizeof(buffer), 0);
 	//Decode the message received
 
+	char commands[3][20];			//the commands
+	char* currPosPtr = buffer;
+	char* nextPosPtr = buffer;
+
+	for (short i = 0; i < 3; ++i)  //max commands = 3
+	{
+		currPosPtr = strchr(currPosPtr + 1, '>');							//find where the > character is: shifts up each time
+		if (currPosPtr == NULL) { break; }									//for < 3 commands
+
+		nextPosPtr = strchr(currPosPtr + 1, '>');
+		if (nextPosPtr == NULL) { break; }									//to not include the last >
+
+		memcpy(commands[i], currPosPtr + 1, (nextPosPtr - currPosPtr) - 1); //copy the length of the command (excluding the start and end >)
+		commands[i][(nextPosPtr - currPosPtr) - 1] = NULL;					//add terminating NULL
+	}
 }
 int ZMQHandler::send()
 {
