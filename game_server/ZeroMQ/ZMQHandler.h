ef53e7fb4efa9ad89d4d9bc7ccdeb6d15c0d8067
Nightwolve <bartstarss@gmail.com>
2020-05-21 11:11:13 +0200

    added ZeroMQ propperly

---
+++ game_server/ZeroMQ/ZMQHandler.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <zmq.h>
+//#include "controller.h"
+class ZMQHandler
+{
+public:
+	ZMQHandler(const char* pub, const char* push);
+	~ZMQHandler();
+	int recv();
+	int send();
+	//controller* myController = NULL;
+private:
+	//vars
+	void* pushPtr, * subPtr, * context;
+
+	//functions
+
+
+};
+
