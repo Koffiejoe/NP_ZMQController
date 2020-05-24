2ba9e6ecde53f1dc751a13288d0db7bd2c3e26c6
Dennis <dennis.merken@gmail.com>
2020-05-24 15:40:15 +0200

    setup main for 2 contr

--- Controller_Server/ConsoleApplication1/ConsoleApplication1/Controller_Server.cpp
+++ Controller_Server/ConsoleApplication1/ConsoleApplication1/Controller_Server.cpp
@@ -20,14 +20,15 @@ int main(int argc, char* argv[])
 	std::cout << "##################################################" << std::endl << std::endl;
 
 	GC* GCController = new GC("COM10");
-	
+	NES* NESController = new NES("COM5");
 	
 
 
 	ZMQHandler myHandler("tcp://benternet.pxl-ea-ict.be:24042", "tcp://benternet.pxl-ea-ict.be:24041");
 	myHandler.controllerList.push_back(GCController);
-
+	myHandler.controllerList.push_back(NESController);
 	myHandler.controllerList.at(0)->updateSpeed = 1000;
+	myHandler.controllerList.at(1)->updateSpeed = 500;
 	while (1)
 	{
 		myHandler.send();
